c9bdf1038f10c68850f439b3a55b95d5d606a06f
github username <diep.christopher@gmail.com>
2018-06-30 19:42:45 -0400

    fizzbuzz solved

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  let x = 1
+
+  while (n > 0) {
+    if (x % 15 === 0) {
+      console.log("fizzbuzz")
+    } else if (x % 5 === 0) {
+      console.log("buzz")
+    } else if (x % 3 === 0) {
+      console.log("fizz")
+    } else {
+      console.log(x)
+    }
+    --n
+    ++x
+  }
+}
 
 module.exports = fizzBuzz;
